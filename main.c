542a5e76869c94899b0f72147296d6f005c87f6d
jacdiste <distefano.1996080@studenti.uniroma1.it>
2024-06-29 16:34:09 +0200

    fd init

--- main.c
+++ main.c
@@ -4,9 +4,36 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/mman.h>
 #include <fcntl.h>
+
 #include "myFAT.h"
 
 int main(int argc, char *argv[]) {
-    return 0;
+
+    FATFileSystem* fs = (FATFileSystem*) malloc(sizeof(FATFileSystem));
+    memset(fs->disk, FREE, sizeof(fs->disk));
+    int fd = open("filesystem", O_RDWR | O_CREAT, 0666);
+    ftruncate(fd, BLOCK_SIZE*BLOCKS_NUMBER);
+    fs->data = mmap(NULL, BLOCK_SIZE*BLOCKS_NUMBER, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    memset(fs->data, '\0', BLOCK_SIZE*BLOCKS_NUMBER);
+    close(fd);
+
+    /* STAMPO DISK E DATA */
+    //int i = 0;
+    //for(i; i < BLOCKS_NUMBER; i++) {
+    //    printf("%d", fs->disk[i]);
+    //}
+
+    //printf("\n%d", i);
+
+    //int j = 0;
+    //for(j; j < BLOCK_SIZE*BLOCKS_NUMBER; j++) {
+    //    printf("%d", fs->data[j]);
+    //}
+
+    //printf("\n%d", j);
+
+    munmap(fs->data, BLOCKS_NUMBER*BLOCK_SIZE);
+    free(fs);
 }
--- myFAT.h
+++ myFAT.h
@@ -1,33 +1,39 @@
+#ifndef MYFAT_H
+#define MYFAT_H
+
 #define MAX_NAME_LENGHT 256
 #define MAX_ENTRIES 100
 #define BLOCKS_NUMBER 1024
 #define BLOCK_SIZE 4096
+#define FREE -1
 
 typedef struct{
     char name[MAX_NAME_LENGHT];
-    int startIndex;
+    int startBlock;
     int size;
     int isDir;
-    int parentDirIndex;
+    int parentDirBlock;
 } DirEntry;
 
 typedef struct{
-    int index;
     int pos;
 }FileHandle;
 
 typedef struct{
-    int FAT[BLOCKS_NUMBER];
+    int disk[BLOCKS_NUMBER];
     DirEntry entries[MAX_ENTRIES];
     char* data;
 } FATFileSystem;
 
+
 void createFile(FATFileSystem* fs, const char* filename);
 void eraseFile(FATFileSystem* fs, const char* filename);
-void write(FATFileSystem* fs, const char* filename, const char* data);
-void read(FATFileSystem* fs, const char* filename, char* data, int size);
-void seek(FATFileSystem* fs, const char* filename, int position);
+void writeFile(FATFileSystem* fs, const char* filename, const char* data);
+void readFile(FATFileSystem* fs, const char* filename, char* data, int size);
+void seekFile(FATFileSystem* fs, const char* filename, int position);
 void createDir(FATFileSystem* fs, const char* dirname);
 void eraseDir(FATFileSystem* fs, const char* dirname);
 void changeDir(FATFileSystem* fs, const char* dirname);
 void listDir(FATFileSystem* fs);
+
+#endif
